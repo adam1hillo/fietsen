deee5825e2bbe7afca9c99026b2610e6b579bd4e
Adam Hillo <adam1hillo@gmail.com>
2024-05-07 12:01:12 +0200

    JUnit

---
+++ src/test/java/be/vdab/fietsen/docenten/DocentRepositoryTest.java
@@ -0,0 +1,27 @@
+package be.vdab.fietsen.docenten;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class DocentRepositoryTest {
+    private static final String DOCENTEN_TABLE = "docenten";
+    private final DocentRepository docentRepository;
+    private final JdbcClient jdbcClient;
+
+    DocentRepositoryTest(DocentRepository docentRepository, JdbcClient jdbcClient) {
+        this.docentRepository = docentRepository;
+        this.jdbcClient = jdbcClient;
+    }
+
+    @Test
+    void countVindtHetJuisteAantalRecords() {
+        assertThat(docentRepository.count())
+                .isEqualTo(JdbcTestUtils.countRowsInTable(jdbcClient, DOCENTEN_TABLE));
+    }
+}
